1dbccc925ac7d7d870f127c7148deda09889ea15
Red-K4tana <ceptor.nill@gmail.com>
2021-10-27 20:10:08 +0300

    Create freeCodeCamp.js

---
+++ FREECODECAMP/freeCodeCamp.js
@@ -0,0 +1,82 @@
+// EXERCISE - Understand String Immutability
+// Setup
+let myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// ------------------------------------------------
+
+// EXERCISE - Use Bracket Notation to Find the First Character in a StringPassed
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// ------------------------------------------------
+
+// EXERCISE - Find the Length of a StringPassed
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// ------------------------------------------------
+
+// EXERCISE - Use Bracket Notation to Find the Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// ------------------------------------------------
+
+// EXERCISE - Use Bracket Notation to Find the Nth-to-Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// ------------------------------------------------
+
+// EXERCISE - Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " very " + myAdjective + " and " + myVerb + " in home " + myAdverb; // Change this line
+// Only change code above this line
+
+// ------------------------------------------------
+
+// EXERCISE - Store Multiple Values in one Variable using JavaScript ArraysPassed
+// Only change code below this line
+const myArray = ['hello', 3];
+
+// ------------------------------------------------
+
+// EXERCISE - Nest one Array within Another Array
+// Only change code below this line
+const myArray = [['fox', 2], ['wolf'], 1];
+
+// ------------------------------------------------
+
+// EXERCISE - Access Array Data with Indexes
+const myArray = [50, 60, 70];
+let myData = myArray[0];
+
+// ------------------------------------------------
+
+// EXERCISE - Modify Array Data With Indexes
+// Setup
+const myArray = [18, 64, 99];
+// Only change code below this line
+myArray[0] = 45;
+
+// ------------------------------------------------
