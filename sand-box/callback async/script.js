0e452e41dd7217769bed02657ea206e52b88e73e
Red-K4tana <ceptor.nill@gmail.com>
2023-04-09 20:58:16 +0300

    promise

--- sand-box/callback async/script.js
+++ sand-box/callback async/script.js
@@ -23,3 +23,39 @@ const fetchData = (userID, callback) => {
 }
 
 /*-------------------------------------------------------------------------*/
+const inputProm = document.querySelector('#input-prom')
+const btnProm = document.querySelector('#btn-prom')
+let strInputProm = ''
+
+inputProm.addEventListener('input', (e) => {
+	strInputProm = e.target.value
+})
+btnProm.addEventListener('click', () => {
+	promFetchData(strInputProm)
+})
+
+const promFetchData = (userID) => {
+	const user = {
+		name: 'Ivan',
+		userID: 'user=',
+	}
+	//Promise вернет либо resolve - и тогда user.userID пойдет в .then и там склеится с alert,
+	// либо reject - и тогда ошибка попадет в catch, и там склеится с alert
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			user.userID = user.userID + userID
+			if (Math.random() < 0.5) {
+				reject('да вашу мать!') // отрицательный результат
+			}
+			resolve(user.userID) // положительный результат
+		}, 300)
+	})
+		// работа с положительным результатом
+		.then(res => {
+			alert('Hi ' + user.name + ' your id - ' + res + '!' + ' вотакенный айдишник теперь')
+		})
+		// работа с отрицательным результатом
+		.catch(err => {
+			alert('error ' + err)
+		})
+}
